b8263be6497c3725c7ec2db7fb47626662a5896a
Sabre <saberfeng@outlook.com>
2017-07-15 20:50:25 +0800

    working on chunk_alloc;allocator half complete

--- allocator.h
+++ allocator.h
@@ -8,7 +8,9 @@
 namespace mini{
 
     template <typename T>
-    struct allocator{
+    class allocator{
+
+    public:
 
         typedef T value_type;
         typedef T* pointer;
@@ -20,11 +22,11 @@ namespace mini{
 
         //pointer allocate(size_type n ,mini::allocator<void>::const_pointer hint=0);
 
-        T* allocate(size_type num){
+        T* _allocate(size_type num){
             return static_cast<T*>(::operator new(num*sizeof(T)));
         }
-        //TODO: use n?
-        void deallocate(pointer ptr,size_type num){
+        //TODO: use num?
+        void _deallocate(pointer ptr,size_type num){
             ::operator delete(static_cast<void*>(ptr));
         }
         void construct(pointer ptr, const_reference value){
@@ -33,6 +35,108 @@ namespace mini{
         void destroy(pointer ptr){
             ptr->~T();
         }
+
+        T* allocate(size_type num){
+            size_type request_bytes=num*sizeof(T);
+            if(request_bytes>128)
+                return _allocate(num);
+
+            obj **free_list_item=free_list;
+            free_list_item+=get_list_index(request_bytes);
+            obj *result=*free_list_item;
+
+            if(result==nullptr){
+                result=refill_lists(round_up(request_bytes));
+            }else{
+                //*free_list_item=static_cast<char*>(*result);
+                (*free_list_item)->next_block=result->next_block;
+            }
+
+        }
+
+
+        void deallocate(pointer ptr,size_type num){
+
+        }
+
+    private:
+
+        static obj* free_list[16];
+        static char* start_free;
+        static char* end_free;
+
+        union obj{
+            union obj *next_block;
+        };
+
+        size_type round_up(size_type bytes){
+            return (bytes+7)&(~7);
+        }
+
+        size_type get_list_index(size_type bytes){
+            return (bytes-1)/8;
+        }
+
+        obj* refill_lists(size_type block_size){
+            size_type num_blocks=20;
+            obj *chunk=(obj*)chunk_alloc(block_size,num_blocks);
+
+            //TODO:handle out of memory
+            if(num_blocks==0)
+                return nullptr;
+            else{
+                obj *current=chunk;
+                obj *next=(obj*)((char*)current+block_size);
+
+                for(size_type i=0;i<num_blocks-1;++i){
+                    current->next_block=next;
+                    current=next;
+                    next=(obj*)((char*)next+block_size);
+                }
+                current->next_block=nullptr;
+
+                obj **free_list_item=free_list;
+                free_list_item+=get_list_index(block_size);
+                *free_list_item=chunk->next_block;
+                return chunk;
+            }
+
+        }
+
+        char* chunk_alloc(size_type block_size,size_type &num_blocks){
+
+            size_type request_bytes=block_size*num_blocks;
+            size_type bytes_left=end_free-start_free;
+            char *result=nullptr;
+
+            if(bytes_left>request_bytes){
+                result=start_free;
+                start_free+=request_bytes;
+                return result;
+            }else if (bytes_left==request_bytes){
+                result=start_free;
+                start_free= (char*)(operator new(2*request_bytes));
+                end_free=start_free+2*request_bytes;
+                return result;
+            }else if(bytes_left>=block_size){
+                num_blocks=bytes_left/block_size;
+                result=start_free;
+                start_free+=num_blocks*block_size;
+                return result;
+            }else{
+                size_type left_fill_index=get_list_index(bytes_left)-1;
+                //put all left memory into proper list
+            }
+            return result;
+        }
+
+    };
+
+    template <typename T> char* allocator<T>::start_free=nullptr;
+    template <typename T> char* allocator<T>::end_free=nullptr;
+    template <typename T> char* allocator<T>::free_list[16]={
+            nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,
+            nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr
     };
 
     //TODO: throw()?
--- main.cpp
+++ main.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
 #include <memory>
+#include <string>
 #include "allocator.h"
 
 using std::cout;
 using std::endl;
 
+void TestAllocator(){
+
+    mini::allocator<int> int_alloc;
+    int *int_ptr=int_alloc.allocate(1);
+    int_alloc.construct(int_ptr,100);
+    cout<<*int_ptr<<endl;
+    int_alloc.destroy(int_ptr);
+    int_alloc.destroy(int_ptr);
+    int_alloc.deallocate(int_ptr,1);
+
+    mini::allocator<std::string> string_alloc;
+    std::string *string_ptr=string_alloc.allocate(2);
+    string_alloc.construct(string_ptr,"first");
+    string_alloc.construct(string_ptr+1,"second");
+    cout<<*string_ptr<<endl<<*(string_ptr+1)<<endl;
+    string_alloc.destroy(string_ptr);
+    string_alloc.destroy(string_ptr+1);
+    string_alloc.deallocate(string_ptr,2);
+}
+
+void TestPointerCast(){
+
+}
+
+
 int main() {
 
-    mini::allocator<int> alloc;
-    int *int_ptr=alloc.allocate(1);
-    alloc.construct(int_ptr,100);
-    cout<<*int_ptr<<endl;
+    //TestAllocator();
+    TestPointerCast();
 
     return 0;
 }
